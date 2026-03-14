cbc07f3aecd9f60bb9e0bc6a46f90fb2e070d412
ampemuycoui-dot <ampe.muyco.ui@phinmaed.com>
2026-03-14 09:47:24 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -7,9 +7,3 @@ self.addEventListener('install', (e) => {
     ]))
   );
 });
-
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
-  );
-});
